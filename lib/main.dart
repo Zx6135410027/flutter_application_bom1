e4b2bd096de850aef418b0cb77a22dcc092af1db
Zx6135410027 <bomtep@hotmail.co.th>
2023-03-07 04:07:10 +0700

    Code V.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_bom1/views/first_ui.dart';
+import 'package:flutter_application_bom1/views/second_ui.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: firstUI(), //เรียกหน้าจอแรก
+      home: secondUI(), //เรียกหน้าจอแรก
     ),
   );
 }
--- lib/views/second_ui.dart
+++ lib/views/second_ui.dart
@@ -10,6 +10,31 @@ class secondUI extends StatefulWidget {
 class _secondUIState extends State<secondUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black12,
+        title: Text(
+          'หน้าแรกUI',
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Colors.amberAccent,
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(),
+    );
   }
 }
